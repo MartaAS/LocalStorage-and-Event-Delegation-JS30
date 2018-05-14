08d0a529be139b3a0846fb89c26e5021ccc95b1c
Marta <martaguilar.soto@gmail.com>
2018-05-14 12:40:33 +0200

    change value checkbox

--- js/main.js
+++ js/main.js
@@ -14,19 +14,19 @@ function addItem(e) {
     text,
     done: false
   };
-  console.log(item);
+
   items.push(item);
   populateList(items, itemList);
   localStorage.setItem('items', JSON.stringify(items));
   this.reset();
 }
 
-function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates.map((plate, i) => {
+function populateList(plates = [], plateList) {
+  plateList.innerHTML = plates.map((plate, i) => {
     return `
     <li>
     <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
-      <label for ="">${plate.text}</label>
+        <label for="item${i}">${plate.text}</label>
     </li>
     `;
   }).join('');
@@ -34,15 +34,14 @@ function populateList(plates = [], platesList) {
 
 function toggleDone(e) {
   if (!e.target.matches('input')) return;
-  const el = e.target;
-  const index = el.dataset.index;
-  items[index].done = !items[index].done;
+  const itemSelected = event.target;
+  const indexOfItemSelected = itemSelected.dataset.index;
+  items[indexOfItemSelected].done = !items[indexOfItemSelected].done;
   localStorage.setItem('items', JSON.stringify(items));
-  populateList(items, itemList);
-  console.log(e.target);
+  populateList(items, itemsList);
 }
 
-populateList(items, itemList);
+populateList(items, itemsList);
 
 
 
